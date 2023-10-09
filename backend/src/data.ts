dece818049bb751de7f0ffc6786ed333253d61bd
Pratik Prabhakarrao <pratikprabhakarraoatwork@gmail.com>
2023-10-10 00:40:20 +0530

    backend first commit 11

---
+++ backend/src/data.ts
@@ -0,0 +1,310 @@
+
+export const sample_foods: any[] = [
+    {
+        id: 1,
+        name: 'Bread Pakoda',
+        cookTime: '20-30 Min',
+        price: 20,
+        favorite: true,
+        origins: ['Mumbai', 'North India'],
+        stars: 2.5,
+        imageUrl: '/assets/images/BREAD PAKODA.jpg',
+        tags: ['FastFood', 'Breakfast', 'Lunch']
+      },
+      {
+        id: 2,
+        name: 'Idly Sambhar',
+        cookTime: '30-50 Min',
+        price: 30,
+        favorite: false,
+        origins: ['South India'],
+        stars: 4.5,
+        imageUrl: '/assets/images/IDLY.jpg',
+        tags: ['FastFood', 'Breakfast', 'Lunch', 'Nutrition']
+      },
+      {
+        id: 3,
+        name: 'Misal Pav',
+        cookTime: '20-30 Min',
+        price: 70,
+        favorite: true,
+        origins: ['Nasik', 'Kolhapur'],
+        stars: 4.5,
+        imageUrl: '/assets/images/misal-pav-recipe.jpg',
+        tags: ['FastFood', 'Breakfast', 'Lunch']
+      },
+      {
+        id: 4,
+        name: 'Pav Bhaji',
+        cookTime: '20-30',
+        price: 20,
+        favorite: false,
+        origins: ['Mumbai'],
+        stars: 3.5,
+        imageUrl: '/assets/images/Pav-Bhaji.webp',
+        tags: ['FastFood', 'Breakfast', 'Lunch']
+      },
+      {
+        id: 5,
+        name: 'Plain Dosa',
+        cookTime: '20-30 Min',
+        price: 40,
+        favorite: true,
+        origins: ['South India'],
+        stars: 5,
+        imageUrl: '/assets/images/plain-dosa.jpg',
+        tags: ['FastFood', 'Breakfast', 'Lunch', 'Nutrition']
+      },
+      {
+        id: 6,
+        name: 'PLAV',
+        cookTime: '20-30 Min',
+        price: 120,
+        favorite: true,
+        origins: ['Mumbai'],
+        stars: 2.5,
+        imageUrl: '/assets/images/PLAV.jpg',
+        tags: ['FastFood', 'Breakfast', 'Lunch']
+      },
+      {
+        id: 7,
+        name: 'Samosa',
+        cookTime: '20-30 Min',
+        price: 15,
+        favorite: true,
+        origins: ['North India'],
+        stars: 1.5,
+        imageUrl: '/assets/images/SAMOSA.jpg',
+        tags: ['FastFood', 'Breakfast']
+      },
+      {
+        id: 8,
+        name: 'Upma',
+        cookTime: '20-30 Min',
+        price: 20,
+        favorite: true,
+        origins: ['South Indian'],
+        stars: 3,
+        imageUrl: '/assets/images/UPMA.jpg',
+        tags: ['FastFood', 'Breakfast', 'Snacks', 'Nutrition']
+      },
+      {
+        id: 9,
+        name: 'Vada Pav',
+        cookTime: '5 Min',
+        price: 10,
+        favorite: true,
+        origins: ['Mumbai'],
+        stars: 2.5,
+        imageUrl: '/assets/images/Vada-Pav-500x500.webp',
+        tags: ['FastFood', 'Breakfast', 'Snacks']
+      },
+      {
+        id: 10,
+        name: 'AlooVadi',
+        cookTime: '50 Min',
+        price: 25,
+        favorite: true,
+        origins: ['Maharashtra', 'Gujrat'],
+        stars: 2.5,
+        imageUrl: 'assets/images/ALOOVADI.jpg',
+        tags: ['FastFood', 'Breakfast', 'Snacks']
+      },
+      {
+        id: 11,
+        name: 'Bedai',
+        cookTime: '50 Min',
+        price: 20,
+        favorite: false,
+        origins: ['Uttar Pradesh'],
+        stars: 2,
+        imageUrl: 'assets/images/BEDAI.jpg',
+        tags: ['FastFood', 'Breakfast', 'Snacks']
+      },
+      {
+        id: 12,
+        name: 'Sevaiya',
+        cookTime: '20 Min',
+        price: 20,
+        favorite: true,
+        origins: ['Maharashtra'],
+        stars: 5,
+        imageUrl: 'assets/images/SEVAI.jpg',
+        tags: ['FastFood', 'Breakfast', 'Snacks', 'Nutrition']
+      },
+      {
+        id: 13,
+        name: 'Chole Bhature',
+        cookTime: '50 Min',
+        price: 30,
+        favorite: false,
+        origins: ['Uttar Pradesh'],
+        stars: 2.5,
+        imageUrl: 'assets/images/CHOLE BHATURE.jpg',
+        tags: ['FastFood', 'Breakfast', 'Snacks']
+      },
+      {
+        id: 14,
+        name: 'Dahi Vada',
+        cookTime: '50 Min',
+        price: 30,
+        favorite: false,
+        origins: ['Punjab'],
+        stars: 4,
+        imageUrl: 'assets/images/DAHI VADA.jpg',
+        tags: ['FastFood', 'Breakfast', 'Snacks']
+      },
+      {
+        id: 15,
+        name: 'Dhokla',
+        cookTime: '50 Min',
+        price: 20,
+        favorite: true,
+        origins: ['Gujrat'],
+        stars: 4,
+        imageUrl: 'assets/images/DHOKLA.jpg',
+        tags: ['FastFood', 'Breakfast', 'Snacks','Nutrition']
+      },
+      {
+        id: 16,
+        name: 'Gulab Jamun',
+        cookTime: '50 Min',
+        price: 20,
+        favorite: true,
+        origins: ['India'],
+        stars: 4,
+        imageUrl: 'assets/images/GULAB JAMUN.jpg',
+        tags: ['Breakfast', 'Sweet']
+      },
+      {
+        id: 17,
+        name: 'Jalebi Fafda',
+        cookTime: '50 Min',
+        price: 20,
+        favorite: true,
+        origins: ['Gujrat'],
+        stars: 1,
+        imageUrl: 'assets/images/JALEBI FAFDA.jpg',
+        tags: ['Breakfast']
+      },
+      {
+        id: 18,
+        name: 'Kachori',
+        cookTime: '50 Min',
+        price: 15,
+        favorite: false,
+        origins: ['Rajasthan'],
+        stars: 0.5,
+        imageUrl: 'assets/images/KACHORI.jpg',
+        tags: ['Breakfast']
+      },
+      {
+        id: 19,
+        name: 'Khandvi',
+        cookTime: '50 Min',
+        price: 25,
+        favorite: false,
+        origins: ['Gujrat'],
+        stars: 0.5,
+        imageUrl: '/assets/images/KHANDVI.jpg',
+        tags: ['Breakfast', 'Snacks']
+      },
+      {
+        id: 20,
+        name: 'chaole kulche',
+        cookTime: '50 Min',
+        price: 25,
+        favorite: false,
+        origins: ['Punjab'],
+        stars: 0.5,
+        imageUrl: '/assets/images/KULCHE.jpg',
+        tags: ['Breakfast', 'Snacks']
+      },
+      {
+        id: 21,
+        name: 'Litti Chokha',
+        cookTime: '50 Min',
+        price: 25,
+        favorite: false,
+        origins: ['Bihar'],
+        stars: 3.5,
+        imageUrl: '/assets/images/LITTI CHOKHA.jpg',
+        tags: ['Breakfast', 'Snacks', 'Lunch']
+      },
+      {
+        id: 22,
+        name: 'Mudhi Mansa',
+        cookTime: '50 Min',
+        price: 25,
+        favorite: false,
+        origins: ['Odisha'],
+        stars: 3.5,
+        imageUrl: '/assets/images/ODIA MUDHI MANSA.jpg',
+        tags: ['Breakfast', 'Snacks', 'Lunch']
+      },
+      {
+        id: 23,
+        name: 'Poori Sabji',
+        cookTime: '50 Min',
+        price: 25,
+        favorite: false,
+        origins: ['North India'],
+        stars: 3.5,
+        imageUrl: '/assets/images/POORI SABJI.jpg',
+        tags: ['Breakfast', 'Snacks', 'Lunch']
+      },
+      {
+        id: 24,
+        name: 'Thepla',
+        cookTime: '50 Min',
+        price: 10,
+        favorite: true,
+        origins: ['Maharashtra', 'Gujrat'],
+        stars: 4.5,
+        imageUrl: '/assets/images/THEPLA.jpg',
+        tags: ['Breakfast', 'Snacks', 'Lunch']
+      },
+      {
+        id: 25,
+        name: 'Ukadpendi',
+        cookTime: '10 Min',
+        price: 20,
+        favorite: true,
+        origins: ['Maharashtra'],
+        stars: 4.5,
+        imageUrl: '/assets/images/UKADPENDI.jpg',
+        tags: ['Breakfast']
+      },
+      {
+        id: 26,
+        name: 'Modak',
+        cookTime: '40 Min',
+        price: 20,
+        favorite: true,
+        origins: ['Maharashtra'],
+        stars: 4.5,
+        imageUrl: '/assets/images/UKDICHE MODAK.jpg',
+        tags: ['Sweet']
+      },
+      {
+        id: 27,
+        name: 'Khaman',
+        cookTime: '40 Min',
+        price: 20,
+        favorite: true,
+        origins: ['Gujrat'],
+        stars: 4.5,
+        imageUrl: '/assets/images/KHAMAN.jpg',
+        tags: ['Breakfast','Nutrition']
+      }
+]
+
+export const sample_tags: any[]  = [
+  {name: 'All', count: 27},
+  {name: 'FastFood', count: 15},
+  {name: 'Breakfast', count: 27},
+  {name: 'Lunch', count: 10},
+  {name: 'Nutrition', count: 6},
+  {name: 'Snacks', count: 14},
+  {name: 'Sweet', count: 2},
+]
